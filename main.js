35194f1bb86aec5af9b822ea2185740d79d96e9a
Melanie Hallowell <melleehall@gmail.com>
2020-02-12 12:37:55 -0700

    Added Economy Pump to Projects as well as made small styling revisions and updated technologies practiced in bio

--- main.js
+++ main.js
@@ -9,4 +9,18 @@ function quizImageSwap (){
     });
 }
 
-$(quizImageSwap)
+function economyPumpImageSwap (){
+    $('.economy-pump-img').mouseenter(function(){
+        $('.economy-pump-img').attr('src', 'EconomyPumpShopView.jpg');
+    });
+    $('.economy-pump-img').mouseleave(function(){
+        $('.economy-pump-img').attr('src', 'EconomyPumpHHIncomeView.jpg');
+    });
+}
+
+function imageSwaps () {
+    quizImageSwap();
+    economyPumpImageSwap();
+}
+
+$(imageSwaps)
